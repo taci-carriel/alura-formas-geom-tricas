6d38c73e851384731b1f88f5b7e0d1afd29fe071
Taciane_ <tacicarriel19@gmail.com>
2025-02-17 21:35:00 -0300

    p5.js

--- p5.js
+++ p5.js
@@ -1,39 +1,4 @@
 function setup() {
-  createCanvas(400,400);
-  }
-function draw() {
-  background(255);
-  fill('#C8ACDD');
-  stroke(100);
-  strokeWeight(4);
-  circle(200, 200, 300); // rosto
-  fill('white');  
-  stroke('#D8A5EB');
-  strokeWeight(1);
-  circle(150, 150, 60); // olho esquerdo
-  circle(250, 150, 60); // olho direito
-  stroke('rgb(140,101,158)');  
-  strokeWeight(4);
-  line(150, 270, 250, 235); // boca
-  fill('rgb(140,101,158)');
-  stroke('rgb(140,101,158)');
-  strokeWeight(1); 
-  triangle(200, 180, 170, 220, 220, 220); // nariz
-  strokeWeight(4);
-  line(123,115,178,113); // sobrancelha esquerda
-  line(225,116,279,106); // sobrancelha direita
-  stroke(0); 
-  strokeWeight(1);
-  circle(150, 150, 10); // pupila esquerda
-  circle(250, 150, 10); // pupila direita
-    
-    if(mouseIsPressed){
-        console.log(mouseX,mouseY);
-    }
-}
-
-
-function setup() {
   createCanvas(400, 400);
 }
 let olhoX;
